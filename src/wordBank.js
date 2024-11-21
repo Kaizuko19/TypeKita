338a95064a05cbe9c395daee45b1b38a7a6e0d65
Daniel Apolonio Bobadilla <106309840+Kaizuko19@users.noreply.github.com>
2024-11-21 19:27:22 +0800

    changed wordbanks

--- src/wordBank.js
+++ src/wordBank.js
@@ -1,9 +1,9 @@
 const wordBank = [
-    "React is a JavaScript library for building user interfaces. It allows developers to create reusable UI components.",
-    "Typing games are a fun way to improve your typing speed and accuracy. They challenge you to type quickly and correctly.",
-    "Frontend development focuses on the user-facing parts of a website or application, including layout, design, and interactions.",
-    "Backend development involves building the server, database, and application logic that powers a website or application.",
-    "Firebase is a platform developed by Google that provides hosting, authentication, and database services for web and mobile applications.",
+    "Falling in love with you was the easiest thing I've ever done. Nothing  matters to me but you. And everyday I'm aware of this. I loved you the day I met you. I love you today, and I will love you for the rest of my life.",
+    "So it's not gonna be easy. It's gonna be really hard, and we're gonna have to work at this every day. But I wanna do that because I want you. I want all of you, forever. You and me. Every day.",
+    "Medicine, law, business, engineering, these are noble pursuits and necessary to sustain life. But poetry, beauty, romance, love, these are what we stay alive for.",
+    "My love, I cannot tell you how thankful I am for our little infinity. I wouldn't trade it for the world. You gave me a forever within the numbered days, and I'm grateful.",
+    "What do you want? You want the moon? Just say the word and I'll throw a lasso around it and pull it down. Hey, that's a pretty good idea. I'll give you the moon.",
   ];
   
   export default wordBank;
